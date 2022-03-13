06948fd07a475e5d3e047c5a1a424a5f48bd5251
Michael P. Jung <michael.jung@terreon.de>
2022-03-13 01:29:05 +0100

    Implement parsing of aseprite cel chunks

--- sprity-core/src/lib.rs
+++ sprity-core/src/lib.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+
+use heck::ToUpperCamelCase;
+
 pub trait Sprite {
     fn name(&self) -> &'static str;
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct AsepriteFile {
+    pub name: String,
+    pub path: PathBuf,
+    pub tags: Vec<String>,
+    pub layers: Vec<String>,
+}
+
+impl AsepriteFile {
+    pub fn load(name: &str, path: &PathBuf) -> Self {
+        let aseprite = aseprite_reader::Aseprite::from_path(path).unwrap();
+        Self {
+            name: name.to_upper_camel_case(),
+            path: path.to_owned(),
+            tags: vec![],
+            layers: vec![],
+        }
+    }
 }
